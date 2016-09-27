f4b6b37bb8a5238d6281b05e6766ff27f0e2e4e4
Joshua Olds <joshua.olds@outlook.com>
2016-09-27 13:06:58 -0600

    Pulled codefoo files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-$(document).foundation();
+
+//Do Not Modify the getMusic function
+function getMusic(){
+  var artist = document.getElementById('artist').value;
+  itunes.getMusicByArtist(artist).then(drawSongs);
+}
+
+
+
+function drawSongs(songList){
+  console.log(songList);
+}
