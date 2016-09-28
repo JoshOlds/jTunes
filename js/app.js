17aa24bad10835b4fb124285ce5b37744c4f408e
Joshua Olds <joshua.olds@outlook.com>
2016-09-28 16:39:44 -0600

    Complete!

--- js/app.js
+++ js/app.js
@@ -1,20 +1,37 @@
 
+var audioArr = []; //Array of audio elements... glocal!
+
 //Do Not Modify the getMusic function
-function getMusic() {
-    var artist = document.getElementById('artist').value;
-    itunes.getMusicByArtist(artist).then(drawSongs);
+function getMusic(event) {
+  event.preventDefault();
+  var artist = document.getElementById('artist').value;
+  itunes.getMusicByArtist(artist).then(drawSongs);
 }
 
 function drawSongs(songList) {
-    console.log(songList);
+  console.log(songList);
 
-    template = '';
-    
+  template = '';
+  count = 0;
+
+  if (songList.length == 0) {
+    template = `
+      <div class="column small-12">
+        <h1 class="j-tunes">No Tracks Found!</h1>
+      </div>`
+  }
+  else {
 
     songList.forEach(function (item) {
+      if (item !== undefined) {
+        var price = item.price.toFixed(2);
+        var collection = item.collection;
+        if(collection === undefined){collection = ""};
+        var title = item.title;
+        if(title === undefined){title = ""};
+
 
         item.albumArt = swapUrlSize(item.albumArt, 400);
-
         template += `
     <div class="small-12 large-4 column card-container">
       <div class="row card-contents text-center align-center">
@@ -22,7 +39,9 @@ function drawSongs(songList) {
           <img class="album-art" src="${item.albumArt}"
             alt="${item.collection} album image">
           <div class="play">
-            
+            <div class="loader">Loading...</div>
+            <audio src="${item.preview}" id="audio${count}">
+            </audio>
           </div>
         </div>
         <div class="small-12 column align-self-top">
@@ -30,18 +49,44 @@ function drawSongs(songList) {
           <h5>${item.collection}</h5>
         </div>
         <div class="small-12 column align-self-bottom">
-          <button class="button success" href="#"><strong class="button-text">$${item.price} - Buy Now!</strong></button>
+          <a class="button success" target="_blank" href="${item.link}"><strong class="button-text">$${item.price} - Buy Now!</strong></a>
         </div>
       </div>
     </div>`
+
+        count++;
+      }
+
     });
+  }
 
-    var songSpace = document.getElementById("song-space");
-    songSpace.innerHTML = template;
+  var songSpace = document.getElementById("song-space");
+  songSpace.innerHTML = template;
+
+  var audio = require('audio'); //Wonky way to create audio components... not sure if this is good or bad lol
+  for (var i = 0; i < 50; i++) {
+    var el = document.getElementById('audio' + i);
+    var temp = audio(el);
+    audioArr.push(temp);
+  }
 }
 
-function swapUrlSize(url, pixels){
-    sizeString = `${pixels}x${pixels}`;
-    var newURL = url.replace("60x60", sizeString);
-    return newURL;
+function swapUrlSize(url, pixels) {
+  sizeString = `${pixels}x${pixels}`;
+  var newURL = url.replace("60x60", sizeString);
+  return newURL;
 }
+
+function activeProgressWheel(id) {
+  console.log("clicked: " + id);
+}
+
+// Code for enter key submission
+document.getElementById("artist").addEventListener("keydown", function (e) {
+  var keyCode = e.keyCode || e.which;
+  if (keyCode === 13) {
+    // enter pressed
+    get();
+    console.log("test");
+  }
+}, false);
--- js/itunes-service.js
+++ js/itunes-service.js
@@ -8,15 +8,19 @@ var itunes = {
       $('#get-music-button').text('LOADING....');
       
       return $.getJSON(apiUrl).then(function(response){
+        console.log(response);
         var songList = response.results.map(function (song) {
+          if(song.kind == "song"){
                   return {
                       title: song.trackName,
                       albumArt: song.artworkUrl60,
                       artist: song.artistName,
                       collection: song.collectionName,
                       price: song.collectionPrice,
-                      preview: song.previewUrl
+                      preview: song.previewUrl,
+                      link: song.trackViewUrl
                     };
+        }
                 })
         $('#get-music-button').text('GET MUSIC');
         return songList;
